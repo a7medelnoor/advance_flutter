220a5f27428462951f7d1dd3d37c85738a95ac55
ahmed <acc.ahmedstudio@gmail.com>
2023-03-02 22:37:21 +0400

    add constants

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,5 +1,11 @@
 class Constants {
   static const String baseUrl = "https://a7medelnoor.mocklab.io/";
   static const String empty  = "";
+  static const String token  = "SEND TOKEN HERE";
   static const int zero = 0;
+  static const int apiTimeOut = 6000;
+  static const String defaultLanguage = "en";
+  static const String arabic = "ar";
+  static const String english = "en";
+
 }
