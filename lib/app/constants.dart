339819f7ff550a021edaff3caeaf7b0175818faf
ahmed <acc.ahmedstudio@gmail.com>
2023-03-02 15:12:35 +0400

    add retrofit API service

---
+++ lib/app/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const String baseUrl = "https://a7medelnoor.mocklab.io/";
+}
---
+++ lib/data/network/app.dart
@@ -0,0 +1,16 @@
+import 'package:advance_flutter/app/constants.dart';
+import 'package:advance_flutter/data/response/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+part 'app.g.dart';
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+factory AppServiceClient(Dio dio,{String baseUrl}) = _AppServiceClient;
+
+@POST("/customers/login")
+  Future<AuthenticationResponse> login(
+    @Field("email") String email,
+    @Field("password") String password
+    );
+}
