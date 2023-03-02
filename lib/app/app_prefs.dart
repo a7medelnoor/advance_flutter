00c9a911ccd3f6129984d50045f9d947c4bd05f6
ahmed <acc.ahmedstudio@gmail.com>
2023-03-02 22:46:56 +0400

    create app shared preferences

---
+++ lib/app/app_prefs.dart
@@ -0,0 +1,21 @@
+import 'package:advance_flutter/presentation/resources/language_manger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppPreferences {
+  SharedPreferences _sharedPreferences;
+
+  AppPreferences(this._sharedPreferences);
+
+  static const String PREFS_KEY_LANG = "PREFS_KEY_LANG";
+
+  Future<String?> getAppLanguage() async {
+    String? language = _sharedPreferences.getString(PREFS_KEY_LANG);
+
+    if (language != null && language.isNotEmpty) {
+      return language;
+    } else {
+      // return default lang
+      LanguageType.ENGLISH.getValue();
+    }
+  }
+}
