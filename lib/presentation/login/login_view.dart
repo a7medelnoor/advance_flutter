32c64bc2d16c2bffc66db3737d897122ca0158b0
ahmed <acc.ahmedstudio@gmail.com>
2023-03-06 14:52:03 +0400

    add view model to the login view

--- lib/presentation/login/login_view.dart
+++ lib/presentation/login/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:advance_flutter/presentation/login/viewmodel/login_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,8 +9,31 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  _bind(){
+    // starting the view model
+    _viewModel.start();
+    // listen to the user input in login edit text
+    // update the view mode with the user name and password
+    _userNameController.addListener(() => _viewModel.setUserName(_userNameController.text));
+    _passwordController.addListener(() => _viewModel.setPassword(_passwordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _viewModel.dispose();
   }
 }
