0839f3eb4d9ed7b83e956d5ab99a164c49341764
ahmed <acc.ahmedstudio@gmail.com>
2023-03-11 14:56:31 +0400

    login screen view

--- lib/presentation/login/login_view.dart
+++ lib/presentation/login/login_view.dart
@@ -1,4 +1,9 @@
 import 'package:advance_flutter/presentation/login/viewmodel/login_viewmodel.dart';
+import 'package:advance_flutter/presentation/resources/assests_manager.dart';
+import 'package:advance_flutter/presentation/resources/color_manager.dart';
+import 'package:advance_flutter/presentation/resources/routes_manager.dart';
+import 'package:advance_flutter/presentation/resources/strings_manager.dart';
+import 'package:advance_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -9,17 +14,20 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+  final LoginViewModel _viewModel = LoginViewModel();
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _fromKey = GlobalKey<FormState>();
 
-  _bind(){
+  _bind() {
     // starting the view model
     _viewModel.start();
     // listen to the user input in login edit text
     // update the view mode with the user name and password
-    _userNameController.addListener(() => _viewModel.setUserName(_userNameController.text));
-    _passwordController.addListener(() => _viewModel.setPassword(_passwordController.text));
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _passwordController
+        .addListener(() => _viewModel.setPassword(_passwordController.text));
   }
 
   @override
@@ -27,10 +35,123 @@ class _LoginViewState extends State<LoginView> {
     _bind();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _getContentWidget();
   }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+        body: Container(
+      padding: const EdgeInsets.only(top: AppPadding.p100),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _fromKey,
+          child: Column(
+            children: [
+              const Center(
+                  child: Image(image: AssetImage(ImageAssets.splashLogo))),
+              const SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsUserNameValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userNameController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.username,
+                          labelText: AppStrings.username,
+                          errorText: (snapshot.data ?? true)? null: AppStrings.usernameError,
+                        ),
+                      );
+                    }),
+              ),
+              const SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsPasswordValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.password,
+                          labelText: AppStrings.password,
+                          errorText: (snapshot.data ?? true)? null: AppStrings.passwordError,
+                        ),
+                      );
+                    }),
+              ),
+              const SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                    stream: _viewModel.outAreAllInputsValid,
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        width: double.infinity,
+                        height: AppSize.s40,
+                        child: ElevatedButton(onPressed:
+
+                            (snapshot.data ?? false )
+                            ? (){
+                          _viewModel.login();
+                        } : null
+                        , child: const Text(AppStrings.login)),
+                      );
+                    }),
+              ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: AppPadding.p8,
+                left: AppPadding.p28, right: AppPadding.p28),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(
+                        context, Routes.forgetPasswordRoute);
+                  },
+                  child: Text(
+                    AppStrings.forgetPassword,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(
+                        context, Routes.registerRoute);
+                  },
+                  child: Text(
+                    AppStrings.notMember,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+              ],
+            )),
+
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
   @override
   void dispose() {
     super.dispose();
