577cddf3e6a04c72234319469ed33f5416deabb0
ahmed <acc.ahmedstudio@gmail.com>
2023-03-11 14:55:34 +0400

    login screen string values

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -12,4 +12,11 @@ class AppStrings {
   static const onBoardingSubTitle3 = "Tut app is an awesome flutter application using clean architecture #3";
   static const onBoardingSubTitle4 = "Tut app is an awesome flutter application using clean architecture #4";
   static const skip = "Skip";
+  static const username = "Username";
+  static const usernameError = "Please enter your username";
+  static const password = "Password";
+  static const passwordError = "Please enter your password";
+  static const login = "Login";
+  static const forgetPassword = "Forget Password";
+  static const notMember = "Not a member? Register now";
 }
